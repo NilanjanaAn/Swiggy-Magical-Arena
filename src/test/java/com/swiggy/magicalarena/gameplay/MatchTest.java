0be6bca7a38920d9cdaf8a95f63c8a4125c6283a
NilanjanaAn <ntnilanjana.06@gmail.com>
2024-06-22 23:23:56 +0530

    Assign unique ID to players in unit tests

--- src/test/java/com/swiggy/magicalarena/gameplay/MatchTest.java
+++ src/test/java/com/swiggy/magicalarena/gameplay/MatchTest.java
@@ -12,7 +12,9 @@ class MatchTest {
     @Test
     void testConductMatchInvalidPlayerNotAlive() {
         player1 = new Player(0, 10, 10);
+        player1.setId(1);
         player2 = new Player(5, 10, 10);
+        player2.setId(2);
         match = new Match(player1, player2);
 
         assertNull(match.conductMatch());
@@ -21,7 +23,9 @@ class MatchTest {
     @Test
     void testConductMatchInvalidPlayersCannotInflictDamage() {
         player1 = new Player(100, 30, 1);
+        player1.setId(1);
         player2 = new Player(100, 6, 5);
+        player2.setId(2);
         match = new Match(player1, player2);
 
         assertNull(match.conductMatch());
@@ -30,7 +34,9 @@ class MatchTest {
     @Test
     void testConductMatchValid() {
         player1 = new Player(100, 50, 50);
+        player1.setId(1);
         player2 = new Player(100, 10, 0);
+        player2.setId(2);
         match = new Match(player1, player2);
 
         assertEquals(player1, match.conductMatch());
